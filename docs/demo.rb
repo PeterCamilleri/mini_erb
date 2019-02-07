31431ef67d76dcaa866274ad878293c5d19ccf9a
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-07 16:25:32 -0500

    Updated the demo.

--- docs/demo.rb
+++ docs/demo.rb
@@ -5,12 +5,10 @@ x     = 42
 $env  = binding
 
 str = "ABCD<%= (1..9).to_a.join %>EFGH"
-puts MiniErb.new(str).result($env)
+puts str, MiniErb.new(str).result($env), ""
 
 str = "<% if x==42 %>Life, the Universe and Everything<% else %>Some stuff<% end %>"
-puts MiniErb.new(str).result($env)
+puts str, MiniErb.new(str).result($env), ""
 
 str = "<% 5.times { |i| %> <%= i+1 %> sheep <% } %>"
-puts MiniErb.new(str).result($env)
-
-
+puts str, MiniErb.new(str).result($env), ""
